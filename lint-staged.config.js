52368472b3623e989b71beedac7d1212f73f09a3
Raymond Ha <raymond@shraymonks.com>
2023-02-08 03:18:00 -0500

    Fix tsc check for lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+export default {
+  '*.{c,}js': ['eslint --fix', 'prettier --write'],
+  '*.ts': (files) => {
+    const filesList = files.join(' ');
+    return [
+      'tsc --noEmit',
+      `eslint --fix ${filesList}`,
+      `prettier --write ${filesList}`,
+    ];
+  },
+  '*.{json,md}': 'prettier --write',
+};
